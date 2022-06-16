f002d5f258ec0e995e4825a0d99357a42d150d77
a2d8a4v <a2d8a4v@gmail.com>
2022-06-16 17:42:40 +0800

    Add new functions for images upload
    Add new functions for images upload, like various type of path for different CPTs or screennow

--- includes/class-images-upload.php
+++ includes/class-images-upload.php
@@ -4,6 +4,7 @@
  * Upload image files as avator
  */
 class IMAGES_UPLOAD {
+
 	private $errors;
 
 	/**
@@ -12,29 +13,52 @@ class IMAGES_UPLOAD {
 	 * @var IMAGES_UPLOAD
 	 */
 	private static $instance;
-	private static $run;
 
 	/**
 	 * Set up the hooks and default values
 	 */
 	public function __construct() {
+
 		$this->errors = False;
 		$this->YANN_root_user_id = 1;
-		$this->customdir = 'members';
-		if (self::$run) {
-			$this->add_hooks();
-		}
+		$this->labname = "NTNUSMIL";
+
+		$this->custom_dirs = array(
+			'profile' => 'members',
+			'cpts' => 'post_page',
+			'default' => '',
+		);
+		$this->images_prefix = array(
+			'profile' => 'A00',
+			'media' => 'A01',
+			'cpts' => 'A01',
+			'default' => '',
+		);
+		$this->accept_media_extension = array(
+			'jpg',
+			'jpeg',
+			'gif',
+			'png',
+			'webp'
+		);
+		$this->accepted_screens = array(
+			'user-edit.php',
+			'profile.php',
+			'post.php',
+			'post-new.php',
+			'upload.php'
+		);
+
+
+		$this->add_hooks();
 	}
 
 	/**
 	 * Registers the plugin.
 	 */
-	public static function register($run) {
-		if ( $run === TRUE ) {
-			if ( null === self::$instance ) {
-				self::$instance = new IMAGES_UPLOAD();
-				self::$run = $run;
-			}
+	public static function register() {
+		if ( null === self::$instance ) {
+			self::$instance = new IMAGES_UPLOAD();
 		}
 	}
 
@@ -44,7 +68,7 @@ class IMAGES_UPLOAD {
 	public function add_hooks() {
 		add_action('admin_init', array( $this, 'YANN_NTNUSMIL_create_upload_dir_for_avator') , 10 , 0 );
 
-		add_filter('wp_handle_upload_prefilter', array( $this , 'YANN_NTNUSMIL_handle_uploadedimage' ) , 10 , 1 );
+		add_filter('wp_handle_upload_prefilter', array( $this , 'IMAGES_UPLOAD_handle_uploaded_image' ) , 10 , 1 );
 		add_filter('wp_handle_upload_prefilter', array( $this, 'YANN_NTNUSMIL_custom_media_library_pre_upload') , 10 , 1 );
 		add_filter('wp_handle_upload', array( $this, 'YANN_NTNUSMIL_custom_media_library_post_upload') , 10 , 1 );
 
@@ -57,15 +81,22 @@ class IMAGES_UPLOAD {
 	 * Get reffer information from $_SERVER
 	 *
 	 * @param object $file file info
+	 * 
+	 * :See @https://stackoverflow.com/questions/4636166/only-variables-should-be-passed-by-reference
+	 * 
+	 * NOTICE: only $_SERVER global variable can provide the url information. DO NOT USE $_REQUEST here
+	 * 
 	 */
-	// @https://stackoverflow.com/questions/4636166/only-variables-should-be-passed-by-reference
-    public function YANN_NTNUSMIL_get_current_page_from_reffer_url($SERVER) {
-		if (!isset($SERVER['HTTP_REFERER'])) {
+    private function IMAGES_UPLOAD___get_current_page_from_reffer_url($_server) {
+
+		// validation
+		if (!isset($_server['HTTP_REFERER'])) {
 			return 'index.php';
 		}
-		$t = explode("?",$SERVER['HTTP_REFERER']);
+
+		$t = explode("?",$_server['HTTP_REFERER']);
 		$p = array_reverse($t);
-		$r = explode("/", array_pop($p));
+		$r = explode('/', array_pop($p));
 		return array_pop( $r );
     }
 
@@ -75,7 +106,7 @@ class IMAGES_UPLOAD {
 	 * @param object $file file info
 	 */
     public function YANN_NTNUSMIL_create_upload_dir_for_avator() {
-		$upload_dir = wp_upload_dir()['basedir'] . "/" . $this->customdir;
+		$upload_dir = wp_upload_dir()['basedir'] . '/' . $this->custom_dirs['profile'];
 		if ( ! is_dir($upload_dir) ) {
 			mkdir( $upload_dir, 0700 );
 		}
@@ -101,32 +132,134 @@ class IMAGES_UPLOAD {
         return $fileinfo;
     }
 
+	private function IMAGES_UPLOAD___validation_screen_now( $pagenow ) {
+
+		// variables
+		$errors = new WP_Error();
+
+		// validation
+		if ( ! in_array( $pagenow , $this->accepted_screens ) ) {
+			$errors->add( __FUNCTION__, __('screen is invalid!', 'YANN_NTNUSMIL') );
+        }
+
+		return $errors;
+	}
+
+	private function IMAGES_UPLOAD___validation_file_type( $_request ) {
+
+		// variables
+		global $variables;
+		$errors = new WP_Error();
+
+		if ( isset( $_request['post_id'] ) ) {
+			$post = get_post($_request['post_id']);
+			$post_type = $post->post_type;
+		}
+
+		global $post;
+		if (! empty($post) && ! isset($post_type) ) {
+			$post_type = $post->post_type;
+		}
+
+		if (! isset($post_type)) {
+			$errors->add( __FUNCTION__, __('validation of file type failed!', 'YANN_NTNUSMIL') );
+		}
+
+		$acceptable_file_types = array_merge(
+			$variables->custom_post_types,
+			$variables->wordpress_default_posttypes
+		);
+
+		if (! in_array($post_type, $acceptable_file_types)) {
+			$errors->add( __FUNCTION__, __('invalid post type!', 'YANN_NTNUSMIL') );
+		}
+
+		return $errors;
+
+	}
+
 	/**
 	 * Add user contact methods
 	 *
 	 * @param object $path file upload path info
 	 */
 	// @https://wordpress.stackexchange.com/questions/195453/different-upload-path-per-file-type
+	// @https://stackoverflow.com/questions/8519968/changing-location-of-uploads-folder-for-custom-post-type-only-not-working
     public function YANN_NTNUSMIL_custom_media_library_custom_upload_dir( $path ) {
 
-		$pagenow = $this->YANN_NTNUSMIL_get_current_page_from_reffer_url($_SERVER);
-		if ( ! in_array( $pagenow , array("user-edit.php", "profile.php") ) ) {
+		// variables
+		global $utils;
+
+		// validation
+		$pagenow = $this->IMAGES_UPLOAD___get_current_page_from_reffer_url($_SERVER);
+		if ( $this->IMAGES_UPLOAD___validation_screen_now( $pagenow )->has_errors() ) {
+			return $path;
+		}
+
+		// validation
+		// we only accept 'jpg', 'jpeg', 'gif', 'png', and 'webp'
+        $extension = substr(
+			strrchr(
+				$_POST['name'],
+				'.'
+			),
+			1
+		);
+
+		// validation
+        if (
+			! empty($path['error'] ) ||
+			! in_array( $extension,
+						$this->accept_media_extension
+					  )
+		   ) {
             return $path;
         }
 
-        $extension = substr(strrchr($_POST['name'],'.'),1);
-        if ( !empty( $path['error'] ) || ! in_array( $extension ,array('jpg','jpeg','gif','png','webp') ) ) {
-            //error or other filetype; do nothing. 
-            return $path; 
-        }
+		// validation
+		// we only accept some specific post type here
+		if ( $this->IMAGES_UPLOAD___validation_file_type($_REQUEST)->has_errors() ) {
+			return $path;
+		}
 
-        $customdir = "/" . $this->customdir;
+		// change routes based on the pagenow we are at
+		switch ($pagenow) {
+
+			// profile
+			case ('user-edit.php'):
+				$customdir = '/' . $this->custom_dirs['profile'];
+				break;
+
+			case ('profile.php'):
+				$customdir = '/' . $this->custom_dirs['profile'];
+				break;
+
+			// post and page
+			case ('post.php'):
+				$customdir = '/' . $this->custom_dirs['cpts'] . '/' . $utils->UTILS_get_post_type($_REQUEST) . '/' . $utils->UTILS_get_post_id($_REQUEST);
+				break;
+
+			case ('post-new.php'):
+				$customdir = '/' . $this->custom_dirs['cpts'] . '/' . $utils->UTILS_get_post_type($_REQUEST) . '/' . $utils->UTILS_get_post_id($_REQUEST);
+				break;
+
+			// media upload
+			case ('upload.php'):
+				$customdir = '/' . $this->custom_dirs['cpts'];
+				break;
+
+			// default
+			default:
+				$customdir = $this->custom_dirs['default'];
+				break;
+				
+		}        
 
         $path['path']    = str_replace($path['subdir'], '', $path['path']); //remove default subdir (year/month)
         $path['url']     = str_replace($path['subdir'], '', $path['url']);      
         $path['subdir']  = $customdir;
         $path['path']   .= $customdir; 
-        $path['url']    .= $customdir;  
+        $path['url']    .= $customdir;
 
 		if ( ! is_dir($path['path']) ) {
 			mkdir( $path['path'], 0700, true );
@@ -135,23 +268,75 @@ class IMAGES_UPLOAD {
         return $path;
     }
 
-	public function YANN_NTNUSMIL_handle_uploadedimage($arr) {
+	public function IMAGES_UPLOAD_handle_uploaded_image( $file_upload_info ) {
 
-		$pagenow = $this->YANN_NTNUSMIL_get_current_page_from_reffer_url($_SERVER);
+		// variables
+		global $variables;
+		$extension = pathinfo(
+			$file_upload_info['name'],
+			PATHINFO_EXTENSION
+		);
+		$extension = strtolower($extension);
 
-		if ( ! in_array( $pagenow , array("user-edit.php","profile.php") ) ) {
-            return $arr;
-        }
-		$ext = pathinfo($arr['name'], PATHINFO_EXTENSION);
-
-		// due to needing to upload thesis pdf file at profile page, should return if is pdf
-		if ( $ext == "pdf" ) {
-			return $arr;
+		// map some hetogenious extension to one
+		$mimetype = $variables->extension_mimetype[$extension];
+		$extension = $variables->mimetype_extension[$mimetype];
+		
+		// validation
+		$pagenow = $this->IMAGES_UPLOAD___get_current_page_from_reffer_url($_SERVER);
+		if ( $this->IMAGES_UPLOAD___validation_screen_now( $pagenow )->has_errors() ) {
+			return $file_upload_info;
 		}
 
-		$arr['name'] = "A00_NTNUSMIL_@NTNUSMIL_" . wp_get_current_user()->user_login . "-" . time() . "@_ori." . $ext;
+		// validation
+		// due to uploading thesis pdf file on profile page, we should give a return if is upload thesis file
+		if ( ! in_array( $variables->mimetype_extension[$file_upload_info['type']],
+						$this->accept_media_extension
+			   )
+		   ) {
+			return $file_upload_info;
+		}
 
-        return $arr;
+		// validation
+		// we only accept some specific post type here
+		if ( $this->IMAGES_UPLOAD___validation_file_type($_REQUEST)->has_errors() ) {
+			return $file_upload_info;
+		}
+
+		switch ($pagenow) {
+
+			// profile
+			case ('user-edit.php'):
+				$new_name = $this->images_prefix['profile'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+
+			case ('profile.php'):
+				$new_name = $this->images_prefix['profile'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+
+			// post and page
+			case ('post.php'):
+				$new_name = $this->images_prefix['cpts'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+
+			case ('post-new.php'):
+				$new_name = $this->images_prefix['cpts'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+
+			// media upload
+			case ('upload.php'):
+				$new_name = $this->images_prefix['media'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+
+			// default
+			default:
+				$new_name = $this->images_prefix['default'] . '_' . $this->labname . '_@' . wp_get_current_user()->user_login . '-' . time() . '@_ori.' . $extension;
+				break;
+				
+		}  
+		$file_upload_info['name'] = $new_name;
+
+        return $file_upload_info;
     }
 
 	/**
@@ -180,4 +365,4 @@ class IMAGES_UPLOAD {
 	// }
 }
 
-IMAGES_UPLOAD::register(TRUE);
+IMAGES_UPLOAD::register();
--- includes/class-utils.php
+++ includes/class-utils.php
@@ -111,4 +111,102 @@ class UTILS {
 		return $this->empty_array;
 	}
 
+	public function UTILS_language_regex( $language ) {
+
+		// validation
+		$this->UTILS_variable_validation('empty', $language, '');
+		$this->UTILS_variable_validation('string', $language, '');
+
+		// return
+		switch (strtolower($language)) {
+			case ('chinese'):
+				return "/[\x{4e00}-\x{9fff}]|[\x{3400}-\x{4DBF}]|[\x{f900}-\x{faff}]|[\x{3100}-\x{312f}]/u";
+				break;
+			case ('japanese'):
+				return "/[\x{3000}-\x{303f}]|[\x{3040}-\x{309f}]|[\x{u30a0}-\x{30ff}]|[\x{ff00}-\x{ff9f}]|[\x{4e00}-\x{9faf}]|[\x{3400}-\x{4dbf}]/u";
+				break;
+			case ('korean'):
+				return "/[\x{ac00}-\x{d7a3}]|[\x{1100}-\x{11ff}]|[\x{3131}-\x{318e}]|[\x{ffa1}-\x{ffdc}]/u";
+				break;
+			case ('english'):
+				return "/[a-zA-Z]/";
+				break;
+			case ('english_and_numbers'):
+				return "/[a-zA-Z0-9]/";
+				break;
+			case ('numbers'):
+				return "/[0-9]/";
+				break;
+			case ('special_characters'):
+				return "/[\'^£$%&*()}{@#~?><>,|=_+¬-]/";
+				break;
+			default:
+				throw new exception('Language is no implemented yet.');
+				break;
+		}
+
+	}
+
+
+	public function UTILS_variable_validation( $type, $variable, $error_message ) {
+		switch ($type) {
+			case ('string'):
+				if (! is_string($variable)) {
+					throw new exception('Variable should be a string type!');
+				}
+				break;
+			case ('array'):
+				if (! is_array($variable)) {
+					throw new exception('Variable should be an array type!');
+				}
+				break;
+			case ('empty'):
+				if (empty($variable)) {
+					throw new exception('Variable is empty!');
+				}
+				break;
+			default:
+				throw new exception('You need to give a type!');
+				break;
+		}
+		return True;
+	}
+
+	public function UTILS_get_post_id($_request) {
+
+		$errors = new WP_Error();
+
+		if ( isset( $_request['post_id'] ) ) {
+			return $_request['post_id'];
+		}
+
+		global $post;
+		if (! empty($post) ) {
+			$post_id = $post->ID;
+			return $post_id;
+		}
+
+		$errors->add(__FUNCTION__, __('post_id does not exist here!', 'YANN_NTNUSMIL'));
+		return $errors;
+	}
+
+	public function UTILS_get_post_type($_request) {
+
+		$errors = new WP_Error();
+
+		if ( isset( $_request['post_id'] ) ) {
+			$post = get_post($_request['post_id']);
+			$post_type = $post->post_type;
+			return $post_type;
+		}
+
+		global $post;
+		if (! empty($post) && ! isset($post_type) ) {
+			$post_type = $post->post_type;
+			return $post_type;
+		}
+
+		$errors->add(__FUNCTION__, __('post_type does not exist here!', 'YANN_NTNUSMIL'));
+		return $errors;
+	}
 }
